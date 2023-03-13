f409c3fe6bb4b0ac65853931a0f228a53b5c65d9
Blake Alderink <blake.alderink@gmail.com>
2023-03-13 15:32:51 -0700

    all changes made and successfully updated application to use Context API

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductsContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
 function App() {
+
+	
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item])
 		// add the given item to the cart
 	};
 
 	return (
+	<ProductContext.Provider value={ {products, addItem} }>
+		<CartContext.Provider value={cart}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
